e5bc30d7ed8e944b261b3cb7cfc172e32989e320
jmotl96 <35705892+jmotl96@users.noreply.github.com>
2021-05-13 13:59:36 -0500

    Initial commit

---
+++ server/src/main/java/edu/wctc/wholesale/controller/OrderController.java
@@ -0,0 +1,11 @@
+package edu.wctc.wholesale.controller;
+
+import edu.wctc.wholesale.repos.WholesaleOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+public class OrderController {
+
+}
---
+++ server/src/main/java/edu/wctc/wholesale/controller/theController.java
@@ -0,0 +1,4 @@
+package edu.wctc.wholesale.controller;
+
+public class theController {
+}
